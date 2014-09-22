ffecbe74ac6961cdc5162661039ac56af689b0db
Nitzan Miron <bug.assembla@bugbug.me>
2014-09-22 14:38:44 +0200

    Added support for Boundary params.json file.

--- elb_plugin.py
+++ elb_plugin.py
@@ -2,8 +2,8 @@ import logging
 import datetime
 import time
 import socket
+import json
 
-import settings
 from elb_metrics import get_elb_metrics
 
 HOSTNAME = socket.gethostname()
@@ -22,16 +22,25 @@ def boundary_report_stat(stat_name, stat_value, stat_source=None, stat_timestamp
         stat_timestamp = unix_time_millis(stat_timestamp)
     print "%s %s %s%s" % (stat_name, stat_value, stat_source, (' %d' % stat_timestamp) if stat_timestamp else '')
 
+def parse_params():
+    with open('param.json') as f:
+        params = json.loads(f.read())
+    if len(params['items']) != 1:
+        raise "Invalid parameters"
+    return params['items'][0]
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, filename=getattr(settings, 'LOG_FILE', None))
+    settings = parse_params()
+
+    logging.basicConfig(level=logging.INFO, filename=settings.get('log_file', None))
 
     while True:
-        data = get_elb_metrics(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+        data = get_elb_metrics(settings['access_key_id'], settings['secret_access_key'])
 
         for lb_name,lb_data in data.items():
             for metric_name,metric_data in lb_data.items():
                 boundary_report_stat('AWS_ELB_' + metric_name, metric_data['Value'], 'ELB_' + lb_name, metric_data['Timestamp'])
 
         print ""
-        time.sleep(float(settings.POLL_INTERVAL) / 1000)
+        time.sleep(float(settings.get("pollInterval", 60*1000) / 1000)
 
