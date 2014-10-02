ade70a5c3f1688fbc29da38ce2082532e35fb42e
Nitzan Miron <bug.assembla@bugbug.me>
2014-10-02 21:48:00 +0300

    Refactored codebase to be more generic and share code with other AWS CloudWatch plugins.  Also merged in changes made to the plugin in the official Boundary repo.

--- elb_plugin.py
+++ elb_plugin.py
@@ -1,85 +1,51 @@
-import logging
-import datetime
-import time
-import socket
-import json
+import boto
+import boto.ec2.elb
+import sys
 
-from elb_metrics import get_elb_metrics
-import boundary_plugin
-import status_store
+from boundary_aws_plugin.cloudwatch_plugin import CloudwatchPlugin
+from boundary_aws_plugin.cloudwatch_metrics import CloudwatchMetrics
 
-'''
-If getting statistics from CloudWatch fails, we will retry up to this number of times before
-giving up and aborting the plugin.  Use 0 for unlimited retries.
-'''
-PLUGIN_RETRY_COUNT = 0
-'''
-If getting statistics from CloudWatch fails, we will wait this long (in seconds) before retrying.
-This value must not be greater than 30 seconds, because the Boundary Relay will think we've
-timed out and terminate us after 30 seconds of inactivity.
-'''
-PLUGIN_RETRY_DELAY = 5
+class ElbCloudwatchMetrics(CloudwatchMetrics):
+    def __init__(self, access_key_id, secret_access_key):
+        return super(ElbCloudwatchMetrics, self).__init__(access_key_id, secret_access_key, 'AWS/ELB')
 
-def get_elb_metrics_with_retries(*args, **kwargs):
-    '''
-    Calls the get_elb_metrics function, taking into account retry configuration.
-    '''
-    retry_range = xrange(PLUGIN_RETRY_COUNT) if PLUGIN_RETRY_COUNT > 0 else iter(int, 1)
-    for _ in retry_range:
-        try:
-            return get_elb_metrics(*args, **kwargs)
-        except Exception as e:
-            logging.error("Error retrieving CloudWatch data: %s" % e)
-            boundary_plugin.report_alive()
-            time.sleep(PLUGIN_RETRY_DELAY)
-            boundary_plugin.report_alive()
+    def get_region_list(self):
+        # Some regions are returned that actually do not support EC2.  Skip those.
+        return [r for r in boto.ec2.elb.regions() if r.name not in ['cn-north-1', 'us-gov-west-1']]
 
-    logging.fatal("Max retries exceeded retrieving CloudWatch data")
-    raise Exception("Max retries exceeded retrieving CloudWatch data")
+    def get_entities_for_region(self, region):
+        elb = boto.connect_elb(self.access_key_id, self.secret_access_key, region=region)
+        return elb.get_all_load_balancers()
 
-def handle_elb_metrics(data, reported_metrics):
-    # Data format:
-    # (RegionId, LoadBalancerName, MetricName) -> [(Timestamp, Value, Statistic), (Timestamp, Value, Statistic), ...]
-    for metric_key, metric_list in data.items():
-        region_id, load_balancer_name, metric_name = metric_key
+    def get_entity_dimensions(self, region, load_balancer):
+        return dict(LoadBalancerName=load_balancer.name)
 
-        for metric_list_item in metric_list:
-            # Do not report duplicate or past samples (note: we are comparing tuples here, which
-            # amounts to comparing their timestamps).
-            if reported_metrics.get(metric_key, None) >= metric_list_item:
-                continue
+    def get_metric_list(self):
+        return (
+            ('HealthyHostCount', 'Average','AWS_ELB_HEALTHY_HOST_COUNT'),
+            ('UnHealthyHostCount', 'Average','AWS_ELB_UNHEALTHY_HOST_COUNT'),
+            ('RequestCount','Sum','AWS_ELB_REQUEST_COUNT'),
+            ('Latency', 'Average','AWS_ELB_LATENCY'),
+            ('HTTPCode_ELB_4XX','Sum','AWS_ELB_HTTP_CODE_4XX'),
+            ('HTTPCode_ELB_5XX','Sum','AWS_ELB_HTTP_CODE_5XX'),
+            ('HTTPCode_Backend_2XX','Sum','AWS_ELB_HTTP_CODE_BACKEND_2XX'),
+            ('HTTPCode_Backend_3XX','Sum','AWS_ELB_HTTP_CODE_BACKEND_3XX'),
+            ('HTTPCode_Backend_4XX','Sum','AWS_ELB_HTTP_CODE_BACKEND_4XX'),
+            ('HTTPCode_Backend_5XX','Sum','AWS_ELB_HTTP_CODE_BACKEND_5XX'),
+            ('BackendConnectionErrors','Sum','AWS_ELB_BACKEND_CONNECTION_ERRORS'),
+            ('SurgeQueueLength', 'Maximum','AWS_ELB_SURGE_QUEUE_LENGTH'),
+            ('SpilloverCount','Sum','AWS_ELB_SPILLOVER_COUNT'),
+        )
 
-            metric_timestamp, metric_value, metric_statistic = metric_list_item
-
-            boundary_plugin.boundary_report_metric('AWS_ELB_' + metric_name, metric_value, 'ELB_' + load_balancer_name, metric_timestamp)
-            reported_metrics[metric_key] = metric_list_item
-
-    status_store.save_status_store(reported_metrics)
+class ElbCloudwatchPlugin(CloudwatchPlugin):
+    def __init__(self):
+        super(ElbCloudwatchPlugin, self).__init__(ElbCloudwatchMetrics, 'AWS_ELB_', 'boundary-plugin-aws-elb-python-status')
 
 if __name__ == '__main__':
-    settings = boundary_plugin.parse_params()
-    reported_metrics = status_store.load_status_store() or dict()
+    if len(sys.argv) > 1 and sys.argv[1] == '-v':
+        import logging
+        logging.basicConfig(level=logging.INFO)
 
-    logging.basicConfig(level=logging.ERROR, filename=settings.get('log_file', None))
-    boundary_plugin.log_metrics_to_file("reports.log")
-
-    # Bring us up to date!  Get all data since the last time we know we reported valid data
-    # (minus 20 minutes as a buffer), and report it now, so that we report data on any time
-    # this plugin was down for any reason.
-    try:
-        earliest_timestamp = max(reported_metrics.values(), key=lambda v: v[0])[0] - datetime.timedelta(minutes=20)
-    except ValueError:
-        # Probably first run or someone deleted our status store file - just start from now
-        logging.error("No status store data; starting data collection from now")
-        pass
-    else:
-        logging.error("Starting historical data collection from %s" % earliest_timestamp)
-        data = get_elb_metrics_with_retries(settings['access_key_id'], settings['secret_access_key'], only_latest=False, start_time=earliest_timestamp, end_time=datetime.datetime.utcnow())
-        handle_elb_metrics(data, reported_metrics)
-        logging.error("Historical data collection complete")
-
-    while True:
-        data = get_elb_metrics_with_retries(settings['access_key_id'], settings['secret_access_key'])
-        handle_elb_metrics(data, reported_metrics)
-        boundary_plugin.sleep_interval()
+    plugin = ElbCloudwatchPlugin()
+    plugin.main()
 
